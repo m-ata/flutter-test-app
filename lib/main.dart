66c08aca79e9725b3a798eefdad5c1fbdb16f59a
Muhammad Ata <muhammad_ata@outlook.com>
2021-02-19 21:48:13 +0300

    flat button, raised button, floating action button learning

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sample App',
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('SampleApp'),
+          title: Text('Flutter App '),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
         ),
         body: Center(
-          child: Text('Hello World from Flutter App'),
-        ),
+          child: FlatButton.icon(
+            icon: Icon(Icons.payment, color: Colors.white,),
+            label: Text('Proceed', style: TextStyle(color: Colors.white),),
+            color: Colors.blue,
+            onPressed: (){},
+          )
       ),
+    ),
     );
   }
 }
