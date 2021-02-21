1c2b990cfddde7be35d512663badae7598771d74
Muhammad Ata <muhammad_ata@outlook.com>
2021-02-21 12:21:47 +0300

    hello world from material app

--- lib/main.dart
+++ lib/main.dart
@@ -2,62 +2,91 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'My App',
-    home: SafeArea(
-      child: MyScaffoldWidget(),
-    ),
+    title: 'Flutter Tutorial',
+    home: TutorialHome()
   ));
 }
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
 
-  final Widget title;
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue),
-      child: Row(
-        children: <Widget>[
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation Menu',
+          onPressed: null,
+        ),
+        title: Text('Example Title'),
+        actions: <Widget>[
           IconButton(
-              icon: (Icon(Icons.menu)),
+              icon: (Icon(Icons.search)),
               onPressed: null,
-              tooltip: 'Navigation Menu',
-          ),
-          Expanded(
-              child: title
-          ),
-          IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null
+              tooltip: 'Search'
           )
         ],
+      ),
+      body: Center(
+        child: Text('Hello World From Material'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: null,
       ),
     );
   }
 }
+// class MyAppBar extends StatelessWidget {
+//   MyAppBar({this.title});
+//
+//   final Widget title;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 56.0,
+//       padding: const EdgeInsets.symmetric(horizontal: 8.0),
+//       decoration: BoxDecoration(color: Colors.blue),
+//       child: Row(
+//         children: <Widget>[
+//           IconButton(
+//               icon: (Icon(Icons.menu)),
+//               onPressed: null,
+//               tooltip: 'Navigation Menu',
+//           ),
+//           Expanded(
+//               child: title
+//           ),
+//           IconButton(
+//               icon: Icon(Icons.search),
+//               tooltip: 'Search',
+//               onPressed: null
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
 
-class MyScaffoldWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Example Title',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          Expanded(
-              child: Center(
-               child: Text('Hello World'),
-              )
-          )
-        ],
-      ),
-    );
-  }
-}
+// class MyScaffoldWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Material(
+//       child: Column(
+//         children: <Widget>[
+//           MyAppBar(
+//             title: Text(
+//               'Example Title',
+//               style: Theme.of(context).primaryTextTheme.headline6,
+//             ),
+//           ),
+//           Expanded(
+//               child: Center(
+//                child: Text('Hello World'),
+//               )
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
